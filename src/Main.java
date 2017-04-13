98609304841d90f668c1bdddf7e05705cb099ec7
stefan2212 <stefan.cernescu94@gmail.com>
2017-04-13 10:17:10 +0100

    Implemented binary search tree

--- src/Main.java
+++ src/Main.java
@@ -70,6 +70,101 @@ class BinaryTree{
         }
         return focusNode;
     }
+
+    public boolean remove(int value){
+        Node focusNode = root;
+        Node parent = root;
+
+        boolean isItALeftChild = true;
+        while(focusNode.value != value){
+            parent = focusNode;
+            if(value< focusNode.value) {
+                isItALeftChild = true;
+                focusNode = focusNode.leftChild;
+            }
+            else {
+                isItALeftChild = false;
+                focusNode = focusNode.rightChild;
+            }
+
+            if(focusNode == null)
+                return false;
+        }
+
+        if (focusNode.leftChild == null && focusNode.rightChild == null){
+            if(focusNode == root)
+                root = null;
+            else if(isItALeftChild){
+                parent.leftChild=null;
+            }
+            else{
+                parent.rightChild=null;
+            }
+        }
+
+        else if(focusNode.rightChild == null){
+            if(focusNode == root)
+            {
+                root = focusNode.leftChild;
+            }
+            else if (isItALeftChild){
+                parent.leftChild = focusNode.leftChild;
+            }
+            else{
+                parent.rightChild = focusNode.rightChild;
+            }
+        }
+
+        else if (focusNode.leftChild == null){
+            if(focusNode == root)
+            {
+                root = focusNode.rightChild;
+            }
+            else if(isItALeftChild){
+                parent.leftChild = focusNode.rightChild;
+            }
+            else
+            {
+                parent.rightChild = focusNode.leftChild;
+            }
+        }
+
+        else{
+
+            Node replacement = getReplacementNode(focusNode);
+            if(focusNode==root){
+                root=replacement;
+            }
+            else if(isItALeftChild){
+                parent.leftChild=replacement;
+            }
+            else
+                parent.rightChild=replacement;
+            replacement.leftChild=focusNode.leftChild;
+        }
+        return true;
+    }
+
+    private Node getReplacementNode(Node replaceNode){
+        Node replacementParent = replaceNode;
+        Node replacement = replaceNode;
+
+        Node focusNode = replaceNode.rightChild;
+
+        while(focusNode!=null){
+            replacementParent=replacement;
+            replacement=focusNode;
+            focusNode=focusNode.leftChild;
+        }
+
+        if(replacement!= replaceNode.rightChild)
+        {
+            replacementParent.leftChild = replacement.rightChild;
+            replacement.rightChild = replaceNode.rightChild;
+        }
+
+        return replacement;
+    }
 }
 
 public class Main {
@@ -82,6 +177,7 @@ public class Main {
         tree.addNode(4);
         tree.addNode(12);
         tree.addNode(5);
+        tree.remove(12);
         tree.preOrderTraversal(tree.root);
     }
 }
